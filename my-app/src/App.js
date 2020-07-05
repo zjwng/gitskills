b68acfdbd7938f28376fa6cbd2f81e0419f93dda
朱佳伟 <7392034+jia_j_w@user.noreply.gitee.com>
2020-07-05 20:47:41 +0800

    add Hello Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Hello from './Hello'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Hello />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
